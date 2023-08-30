4c03241414711e398582b6c284720d9ac12d0f3e
Marcella Costa <marcellafccosta@gmail.com>
2023-08-30 18:15:45 -0300

    enunciado ex8

---
+++ PUC/Lista PM/ex8/Program.java
@@ -0,0 +1,19 @@
+// Considere a Matriz A abaixo:
+
+// 5 10 7 8
+// 13 4 1 3
+// 9 2 6 5
+// 10 7 4 9
+
+// a) Implemente um algoritmo que exiba a matriz transposta AT
+// b) Implemente um algoritmo que exiba a soma dos quadrados da diagonal
+// secundária
+// c) Implemente um algoritmo que faça a multiplicação desta matriz por uma constante C
+// d) Implemente um algoritmo que inverta a 3ª linha com a 3ª coluna
+
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+}
